c9e10c29a0bcecdda4298073645ef5928e0ffa24
Krzysztof Stopka <stopkakrzysztof1@gmail.com>
2023-04-21 00:59:07 +0200

    updated

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-function addClassOnClick(elements, className) {
-    elements.forEach((element) => {
-      element.addEventListener('click', () => {
-        element.classList.add(className);
-      });
-    });
-  }
+
+  function addClass(element, className) {
+    element.classList.add(className);
+  };
+
+
+function checkWin(){
+
+}
 
 const boxes = document.querySelectorAll('div div');
-addClassOnClick(boxes, 'circleActive');
-//addClassOnClick(boxes, 'crossActive');
+
+  boxes.forEach((box) => {
+    box.addEventListener('click', () => {
+      addClass(box, 'circleActive');
+      boxes.forEach((otherBox) => {
+        if (!otherBox.classList.contains('circleActive')) {
+          addClass(otherBox, 'crossActive');
+        }
+      });
+    });
+  });
+  
